4081e4e1314bb3af7cce3b359f93df3101674303
Eze <ezequiel.n.villa@gmail.com>
2023-10-05 18:57:52 -0300

    test before/after each

--- app/test/index.test.ts
+++ app/test/index.test.ts
@@ -3,27 +3,46 @@ import { dbClient } from "../../internal/db/pg";
 process.env.NODE_ENV = "test";
 
 beforeEach((done) => {
-  //
-  dbClient.query(`
+  dbClient.query(
+    `
     DELETE FROM "content";
     DELETE FROM "comment";
     DELETE FROM "article";
     DELETE FROM "blog";
     DELETE FROM "user";
     DELETE FROM "auth";
-  `);
-  done();
+  `,
+    (error, result) => {
+      if (error) {
+        // Handle the error appropriately
+        console.error("Error in beforeEach:", error);
+      }
+      console.log("DONE");
+      // Signal that the cleanup is complete
+      done();
+    }
+  );
 });
 
 afterEach((done) => {
-  dbClient.query(`
+  dbClient.query(
+    `
     DELETE FROM "content";
     DELETE FROM "comment";
     DELETE FROM "article";
     DELETE FROM "blog";
     DELETE FROM "user";
     DELETE FROM "auth";
-  `);
-  done();
-  //
+  `,
+    (error, result) => {
+      if (error) {
+        // Handle the error appropriately
+        console.error("Error in afterEach:", error);
+      }
+      console.log("DONE");
+
+      // Signal that the cleanup is complete
+      done();
+    }
+  );
 });
