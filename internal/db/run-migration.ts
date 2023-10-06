94f3aa475e855163a904080056ecd186885a9e9a
Eze <ezequiel.n.villa@gmail.com>
2023-10-06 09:45:16 -0300

    improve yml

--- internal/db/run-migration.ts
+++ internal/db/run-migration.ts
@@ -3,55 +3,27 @@ import path from "path";
 import { dbClient } from "./pg";
 import dotenv from "dotenv";
 
-export async function runMigration() {
+export function runMigration() {
   dotenv.config();
   const migrationDir = path.join(__dirname, "./../migrations");
-  const migrationFiles = fs.readdirSync(migrationDir);
-  for (const fileName of migrationFiles) {
+  fs.readdirSync(migrationDir).forEach((fileName) => {
     const filePath = path.join(migrationDir, fileName);
     const sql = fs.readFileSync(filePath, "utf-8");
 
-    try {
-      await executeMigration(sql, fileName);
-      console.log(`Migration executed successfully: ${fileName}`);
-      continue;
-    } catch (err) {
-      console.error(`Error executing migration: ${fileName}`);
-      console.error(err);
-      // Handle the error as needed
-    }
-  }
-
-  return;
-  // fs.readdirSync(migrationDir).forEach(async (fileName) => {
-  //   const filePath = path.join(migrationDir, fileName);
-  //   const sql = fs.readFileSync(filePath, "utf-8");
-  //   try {
-  //     await executeMigration(sql, fileName);
-  //     console.log(`Migration executed successfully: ${fileName}`);
-  //   } catch (err) {
-  //     console.error(`Error executing migration: ${fileName}`);
-  //     console.error(err);
-  //     // Handle the error as needed
-  //   }
-  // dbClient.query(sql, (err, result) => {
-  //   if (err) {
-  //     console.error(`Error executing migration: ${fileName}`);
-  //     console.error(err);
-  //   } else {
-  //     console.log(`Migration executed successfully: ${fileName}`);
-  //   }
-  // });
-  // });
-}
-function executeMigration(sql: string, fileName: string) {
-  return new Promise((resolve, reject) => {
     dbClient.query(sql, (err, result) => {
       if (err) {
-        reject(err);
+        console.error(`Error executing migration: ${fileName}`);
+        console.error(err);
       } else {
-        resolve(result);
+        console.log(`Migration executed successfully: ${fileName}`);
       }
     });
   });
+  process.on("exit", (code) => {
+    console.log(`Script exiting with code: ${code}`);
+    // Perform any cleanup or finalization tasks here, if needed
+    // This code will run when the script is about to exit
+  });
+  // Terminate the process explicitly
+  process.exit(); // This will exit the script
 }
--- sync.ts
+++ sync.ts
@@ -1,11 +1,3 @@
 import { runMigration } from "./internal/db/run-migration";
 
-runMigration().then(() => {
-  process.on("exit", (code) => {
-    console.log(`Script exiting with code: ${code}`);
-    // Perform any cleanup or finalization tasks here, if needed
-    // This code will run when the script is about to exit
-  });
-  // Terminate the process explicitly
-  process.exit(); // This will exit the script
-});
+runMigration();
