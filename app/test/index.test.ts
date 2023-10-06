41ba416d25a8ac605ff534d084419b3546a9a329
Eze <ezequiel.n.villa@gmail.com>
2023-10-06 11:05:54 -0300

    before/after in index test

--- app/test/index.test.ts
+++ app/test/index.test.ts
@@ -2,7 +2,8 @@ import { dbClient } from "../../internal/db/pg";
 
 process.env.NODE_ENV = "test";
 
-beforeEach((done) => {
+beforeEach(async () => {
+  await dbClient.query("BEGIN");
   dbClient.query(
     `
     DELETE FROM "content";
@@ -11,34 +12,12 @@ beforeEach((done) => {
     DELETE FROM "blog";
     DELETE FROM "user";
     DELETE FROM "auth";
-  `,
-    (error, result) => {
-      if (error) {
-        console.error("Error in beforeEach:", error);
-      }
-      console.log("DONE");
-
-      done();
-    }
+  `
   );
 });
 
-afterEach((done) => {
-  dbClient.query(
-    `
-    DELETE FROM "content";
-    DELETE FROM "comment";
-    DELETE FROM "article";
-    DELETE FROM "blog";
-    DELETE FROM "user";
-    DELETE FROM "auth";
-  `,
-    (error, result) => {
-      if (error) {
-        console.error("Error in afterEach:", error);
-      }
-      console.log("DONE");
-      done();
-    }
-  );
+afterEach(async () => {
+  // Rollback the transaction to undo any changes made during the test
+  await dbClient.query("ROLLBACK");
+  console.log("ROLLBACK");
 });
